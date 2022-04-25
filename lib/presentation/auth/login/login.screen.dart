4abdf37ed0fc1ad2f9e5b97b586df43527bd30b4
ankit.miyan <ankit.miyan@techspian.com>
2022-04-25 11:09:55 +0530

    UI change

--- lib/presentation/auth/login/login.screen.dart
+++ lib/presentation/auth/login/login.screen.dart
@@ -9,7 +9,7 @@ class LoginScreen extends GetView<LoginController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('LoginScreen'),
+        title: Text('Login Screen'),
         centerTitle: true,
       ),
       body: Center(
